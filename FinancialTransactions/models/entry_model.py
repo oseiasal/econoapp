3c30c17dae4e92c1675dea4d3f8f14c7a3eb8149
Oseias Nascimento <oseiasal@gmail.com>
2023-10-08 16:03:37 -0300

    alterar data

--- FinancialTransactions/models/entry_model.py
+++ FinancialTransactions/models/entry_model.py
@@ -1,14 +1,14 @@
 from django.db import models
+from django.utils import timezone
 from .category_model import Category
 
 
 class Entry(models.Model):
     description = models.CharField(max_length=100)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
-    date = models.DateField()
+    date = models.DateField(default=timezone.now)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.description
-
 
